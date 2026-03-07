9c01b370b07e93e1fb66620e451c2e8a5e9f487d
Raj-Talukder-debug <rajtalukder19@gmail.com>
2026-03-07 06:44:45 +0600

    login functionality complete

--- script/login.js
+++ script/login.js
@@ -1,1 +1,21 @@
-console.log("i AM Log in")
+document.getElementById("login-btn").addEventListener('click', ()=> {
+
+    // 1 get the user name
+    const inputUserName = document.getElementById("input-userName");
+    const userName = inputUserName.value;
+    
+    const inputPassword  = document.getElementById("input-password");
+    const password = inputPassword.value;
+    
+    if(userName == "admin" && password == "admin123"){
+        alert("Sign in successful");
+
+        window.location.assign("/home.html");
+    }
+    else{
+        alert("Invalid Information");
+        return;
+    }
+
+
+});
